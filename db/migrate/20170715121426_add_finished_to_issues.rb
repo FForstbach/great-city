8fa0db97703a13b8b91966b1bc7ebf0f86058087
Fredrik Forstbach <fredrik.forstbach@gmail.com>
2017-07-15 14:14:51 +0200

    adds styles and new field finished

---
+++ db/migrate/20170715121426_add_finished_to_issues.rb
@@ -0,0 +1,5 @@
+class AddFinishedToIssues < ActiveRecord::Migration[5.0]
+  def change
+    add_column :issues, :active, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170714185244) do
+ActiveRecord::Schema.define(version: 20170715121426) do
 
   create_table "issues", force: :cascade do |t|
     t.string   "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170714185244) do
     t.datetime "photo_updated_at"
     t.float    "latitude"
     t.float    "longitude"
+    t.boolean  "active"
   end
 
   create_table "users", force: :cascade do |t|
