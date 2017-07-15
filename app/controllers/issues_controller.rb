5807337e6c05be6ce2320eedf425ec6a6f8f6076
Fredrik Forstbach <fredrik.forstbach@gmail.com>
2017-07-15 15:18:37 +0200

    adds styles

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -76,6 +76,6 @@ class IssuesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def issue_params
-      params.require(:issue).permit(:title, :description, :photo)
+      params.require(:issue).permit(:title, :description, :photo, :votes, :active)
     end
 end
