f6234d6535966675003ca1957f9d9396558752f0
Fredrik Forstbach <fredrik.forstbach@gmail.com>
2017-07-19 10:31:56 +0200

    chages DB to PostgreSQL to prepare for push to Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170715122048) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "issues", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
